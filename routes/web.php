93bbe945ae9aa8ab85bd9a5150844c6346acd770
firdausfikriii <firdaus24ti@mahasiswa.pcr.ac.id>
2025-10-14 23:49:34 +0700

    Template-Admin Volt

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
 
 
 Route::get('/', function () {
@@ -40,3 +41,4 @@ Route::get('/matakuliah/show/{param1}', [MatakuliahController::class, 'show']);
 Route::get('/home', [HomeController::class, 'index']) ->name('home');
 Route::post('question/store', [QuestionController::class, 'store'])->name('question.store');
 
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
